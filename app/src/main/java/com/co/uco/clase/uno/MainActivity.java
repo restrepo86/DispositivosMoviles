8cfbbe9e95cfddb1f190b21fa4d3db335b6a4092
juanftech <juan.restrepo@techandsolve.com>
2018-08-14 20:07:18 -0500

    se agrega evento onClick a boton Aceptar

--- app/src/main/java/com/co/uco/clase/uno/MainActivity.java
+++ app/src/main/java/com/co/uco/clase/uno/MainActivity.java
@@ -2,12 +2,31 @@ package com.co.uco.clase.uno;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText nombreTxt;
+    private Button btnAceptar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nombreTxt = (EditText) findViewById(R.id.nombreTxt);
+        btnAceptar = (Button) findViewById(R.id.btnAceptar);
+        btnAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nombre = nombreTxt.getText().toString();
+                Toast.makeText(getApplicationContext(), "hola" + nombre, Toast.LENGTH_SHORT);
+                nombreTxt.setText("");
+            }
+        });
+
     }
 }
